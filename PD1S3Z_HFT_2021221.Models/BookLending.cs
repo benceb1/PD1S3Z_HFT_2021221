0b05f2f260aac5761a86533213b0ae11537121fd
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-07 15:41:41 +0100

    Added BookLending class

---
+++ PD1S3Z_HFT_2021221.Models/BookLending.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Models
+{
+    [Table("book_lending")]
+    public class BookLending
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool Active { get; set; }
+
+        public virtual Library Library { get; set; }
+
+        public virtual Borrower Borrower { get; set; }
+
+        public int LibraryId { get; set; }
+
+        public int BorrowerId { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Book> Books { get; set; }
+
+        public BookLending()
+        {
+            Books = new HashSet<Book>();
+        }
+    }
+}
