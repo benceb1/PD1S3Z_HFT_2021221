3acc55ad19f52d5f5166b7dd6a60c301ed7ececf
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-07 15:41:52 +0100

    Added Book class

---
+++ PD1S3Z_HFT_2021221.Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Models
+{
+    [Table("book")]
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Publishing { get; set; }
+
+        [NotMapped]
+        public virtual BookLending BookLending { get; set; }
+
+        [NotMapped]
+        public virtual Library Library { get; set; }
+
+        [NotMapped]
+        public virtual Borrower Borrower { get; set; }
+
+        public int LibraryId { get; set; }
+
+    }
+}
