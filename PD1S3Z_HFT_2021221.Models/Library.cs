4765e315af603a66676a6019bf32d49eedc2b8a6
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-07 15:41:09 +0100

    added Library class

---
+++ PD1S3Z_HFT_2021221.Models/Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Models
+{
+    [Table("library")]
+    public class Library
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [MaxLength(5)]
+        public int BookCapacity { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Book> Books { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<BookLending> BookLendings { get; set; }
+
+        // books
+
+        public Library()
+        {
+            Books = new HashSet<Book>();
+            BookLendings = new HashSet<BookLending>();
+        }
+    }
+}
