f9a03329e21e0e15117fdb151c6d0e63bac96a90
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-07 21:10:21 +0100

    base repository interface added

---
+++ PD1S3Z_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Insert(T entity);
+        bool Remove(int id);
+    }
+}
