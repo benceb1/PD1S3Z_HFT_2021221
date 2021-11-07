67567af5bbba045a2b293a217e46822a5b0bca3b
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-07 15:41:23 +0100

    added Borrower class

---
+++ PD1S3Z_HFT_2021221.Models/Borrower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Models
+{
+    [Table("borrower")]
+    public class Borrower
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // bronze, silver, gold
+        public string MembershipLevel { get; set; }
+        public DateTime StartOfMembership { get; set; }
+        public int Age { get; set; }
+        public int NumberOfBooksRead { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Book> Books { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<BookLending> BookLendings { get; set; }
+
+        public Borrower()
+        {
+            BookLendings = new HashSet<BookLending>();
+            Books = new HashSet<Book>();
+        }
+
+    }
+}
