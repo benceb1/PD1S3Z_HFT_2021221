117c1689eae4ead9e96999c54391e0fa6c1de2fd
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-07 21:11:27 +0100

    added entity repository interfaces

---
+++ PD1S3Z_HFT_2021221.Repository/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using PD1S3Z_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Repository
+{
+    interface IBookRepository : IRepository<Book>
+    {
+        void Update(int id, Book book);
+    }
+}
