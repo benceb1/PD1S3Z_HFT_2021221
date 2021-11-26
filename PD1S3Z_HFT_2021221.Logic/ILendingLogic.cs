d822c298915705e341b009f0e883a8ddd438ef85
Balogh Bence <bence.b1.1.2@gmail.com>
2021-11-26 20:15:02 +0100

    added logic interfaces

---
+++ PD1S3Z_HFT_2021221.Logic/ILendingLogic.cs
@@ -0,0 +1,22 @@
+﻿using PD1S3Z_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1S3Z_HFT_2021221.Logic
+{
+    public interface ILendingLogic
+    {
+        Lending StartLending(int borrowerId, List<Book> books, int libraryId, int lendingWeeks);
+
+        Lending EndLending(Lending lending, int libraryId);
+
+        IList<Lending> GetActiveLendings();
+
+        IList<Lending> GetAllLendings();
+
+        IList<Lending> GetLateLendings();
+    }
+}
